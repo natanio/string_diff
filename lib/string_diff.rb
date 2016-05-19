f58cd2e435283df453acf9612aa9f50b6947bd3b
natanio <natanio@gmail.com>
2016-05-19 17:51:45 +0900

    work on passing tests

--- lib/string_diff.rb
+++ lib/string_diff.rb
@@ -46,7 +46,38 @@ module StringDiff
         end
 
         if index.nil?
-          array1.insert(-1, "<span class='insertion'>#{v}</span>")
+          puts "Index is nil"
+          puts array1.to_s
+
+          # If there is punctuation after a deletion, we need to make sure the
+          # insertion is added before the punctuation.
+          if (!(array1.last.scan(/(?<='>).*(?=<\/)/)[0] =~ (/[[:punct:]]/)).nil? || (array1.last =~ (/[[:punct:]]/))) && array1[-2].include?("<span class='deletion'")
+            puts "Block 1"
+            array1.insert(-2, "<span class='insertion'>#{v}</span>")
+          elsif array2.find_index(v) <= (PragmaticTokenizer::Tokenizer.new(downcase: false).tokenize(string1).count)
+            puts "Block 2"
+
+            # Count how many insertions up to the original position
+            insertions_count = 0
+            deletions_count = 0
+            for i in 0...(array2.find_index(v)+1) do
+              puts array2.find_index(v)
+              puts i
+              puts v
+              puts array1[i].to_s
+              puts "array1 insertions test is #{array1[i].include?("<span class='insertion'")}"
+              insertions_count += 1 if array1[i].include?("<span class='insertion'")
+              deletions_count += 1 if array1[i].include?("<span class='deletion'")
+            end
+            puts "Insertion count: #{insertions_count}"
+            puts "Deletion count: #{deletions_count}"
+            puts "index: #{array2.find_index(v)}"
+            array1.insert(((array2.find_index(v) + insertions_count + deletions_count) - 1), "<span class='insertion'>#{v}</span>")
+          else
+            # Otherwise we put it on the end.
+            puts "Block 3"
+             array1.insert(-1, "<span class='insertion'>#{v}</span>")
+          end
         else
           array1.insert(index + 1, "<span class='insertion'>#{v}</span>")
         end
@@ -86,15 +117,30 @@ module StringDiff
         if i == 0
           string += token
         else
+          puts token
           if token.include?("<span")
             if token.scan(/(?<='>).*(?=<\/)/)[0] !~ /[[:punct:]]/ || string1.include?(" #{token.scan(/(?<='>).*(?=<\/)/)[0]}")
               string += " #{token}"
+            elsif !( token.scan(/(?<='>).*(?=<\/)/)[0] =~ (/[']/) ).nil?
+              if string.scan(/[']/).empty?
+                string += " #{token}#{array1[i+1]}"
+                array1.slice!(i+1)
+              else
+                string += token
+              end
             else
               string += token
             end
           else
-            if token !~ /[[:punct:]]/ || string1.include?(" #{token.scan(/(?<='>).*(?=<\/)/)[0]}")
+            if token !~ /[[:punct:]]/
               string += " #{token}"
+            elsif !( token =~ (/[']/) ).nil?
+              if string.scan(/[']/).empty?
+                string += " #{token}#{array1[i+1]}"
+                array1.slice!(i+1)
+              else
+                string += token
+              end
             else
               string += token
             end
@@ -106,3 +152,53 @@ module StringDiff
 
   end
 end
+
+
+# def annotate_insertions(insertions, array1, array2)
+#   insertions.each_with_index do |v, i|
+#     if array2.find_index(v) == 0
+#       index = 0
+#     else
+#       insertion_position = array2.find_index(v) + i - 1
+#       index = array1.find_index(array2[insertion_position])
+#     end
+
+#     if index.nil?
+#       puts "Index is nil"
+#       # If there is punctuation after a deletion, we need to make sure the
+#       # insertion is added before the punctuation.
+#       if !(array1.last.scan(/(?<='>).*(?=<\/)/)[0] =~ (/[[:punct:]]/)).nil? && array1[-2].include?("<span class='deletion'")
+#         puts "Block 1"
+#         array1.insert(-2, "<span class='insertion'>#{v}</span>")
+#       elsif array2.find_index(v) >= (PragmaticTokenizer::Tokenizer.new(downcase: false).tokenize(string1).count -1)
+#         puts "Block 2"
+#         array1.insert(-1, "<span class='insertion'>#{v}</span>")
+#       else
+#         # Otherwise we put it on the end.
+#         puts "Block 3"
+#         # Count how many insertions up to the original position
+#         insertions_count = 0
+#         for i in 0...(array2.find_index(v)+1) do
+#           puts array2.find_index(v)
+#           puts i
+#           puts v
+#           puts array1[i].to_s
+#           puts "array1 insertions test is #{array1[i].include?("<span class='insertion'")}"
+#           insertions_count += 1 if array1[i].include?("<span class='insertion'")
+#         end
+#         puts "Insertion count: #{insertions_count}"
+#         array1.insert((array2.find_index(v) + insertions_count), "<span class='insertion'>#{v}</span>")
+
+
+#         # puts array1.to_s
+#         # puts array1.count
+#         # puts v
+#         # array1.insert((array1.count -1), "<span class='insertion'>#{v}</span>")
+#       end
+#     else
+#       array1.insert(index + 1, "<span class='insertion'>#{v}</span>")
+#     end
+#   end
+
+#   array1
+# end
--- spec/string_diff_spec.rb
+++ spec/string_diff_spec.rb
@@ -1,3 +1,4 @@
+#encoding: utf-8
 require 'spec_helper'
 
 describe StringDiff do
@@ -72,14 +73,15 @@ describe StringDiff do
   it 'should not add space before colon' do
     string_1 = "Otros beneficios:"
     string_2 = "Otras ventajas:"
+
     sd = StringDiff::Diff.new(string_1, string_2).diff
 
-    expect(sd).to eq("Otros <span class='deletion'>beneficios</span> <span class='insertion'>ventajas</span>:")
+    expect(sd).to eq("<span class='deletion'>Otros</span> <span class='insertion'>Otras</span> <span class='deletion'>beneficios</span> <span class='insertion'>ventajas</span>:")
   end
 
   it 'should not add spaces between commas and single quotes' do
     string_1 = "Nota: si su presupuesto está por debajo de la 'tarifa de no molestar' de este traductor, su mensaje no será entregado."
-    string_1 = "Nota: si su presupuesto está por debajo de la 'tarifa de no molestar' de este traductor, su mensaje no será entregado."
+    string_2 = "Nota: si su presupuesto está por debajo de la 'tarifa de no molestar' de este traductor, su mensaje no será entregado."
 
     sd = StringDiff::Diff.new(string_1, string_2).diff
 
@@ -88,7 +90,7 @@ describe StringDiff do
 
   it 'should always show insertion immediately after its deletion' do
     string_1= "Seleccione su presupuesto"
-    string_1 = "Selecciona tu presupuesto"
+    string_2 = "Selecciona tu presupuesto"
 
     sd = StringDiff::Diff.new(string_1, string_2).diff
 
@@ -97,11 +99,11 @@ describe StringDiff do
 
   it 'should add a deletion directly before a word that replaces it' do
     string_1= "Traductor mensaje de"
-    string_1 = "Envía el mensaje al traductor"
+    string_2 = "Envía el mensaje al traductor"
 
     sd = StringDiff::Diff.new(string_1, string_2).diff
 
-    expect(sd).to eq("<span class='deletion'>Traductor</span> <span class='insertion'>Envía</span> mensaje <span class='deletion'>de</span> <span class='insertion'>el</span> <span class='insertion>al</span> <span class=insertion'>traductor</span>")
+    expect(sd).to eq("<span class='deletion'>Traductor</span> <span class='insertion'>Envía</span> <span class='insertion'>el</span> mensaje <span class='deletion'>de</span> <span class='insertion'>al</span> <span class='insertion'>traductor</span>")
   end
 
 end
