81f46c9b2e3863d47f20736815fdb9df361c15a0
natanio <natanio@gmail.com>
2016-05-19 10:33:08 +0900

    add new tests

--- spec/string_diff_spec.rb
+++ spec/string_diff_spec.rb
@@ -69,4 +69,39 @@ describe StringDiff do
     expect(sd).to eq("<span class='deletion'>hello</span> <span class='insertion'>Hello</span> world")
   end
 
+  it 'should not add space before colon' do
+    string_1 = "Otros beneficios:"
+    string_2 = "Otras ventajas:"
+    sd = StringDiff::Diff.new(string_1, string_2).diff
+
+    expect(sd).to eq("Otros <span class='deletion'>beneficios</span> <span class='insertion'>ventajas</span>:")
+  end
+
+  it 'should not add spaces between commas and single quotes' do
+    string_1 = "Nota: si su presupuesto está por debajo de la 'tarifa de no molestar' de este traductor, su mensaje no será entregado."
+    string_1 = "Nota: si su presupuesto está por debajo de la 'tarifa de no molestar' de este traductor, su mensaje no será entregado."
+
+    sd = StringDiff::Diff.new(string_1, string_2).diff
+
+    expect(sd).to eq("Nota: si su presupuesto está por debajo de la 'tarifa de no molestar' de este traductor, su mensaje no será entregado.")
+  end
+
+  it 'should always show insertion immediately after its deletion' do
+    string_1= "Seleccione su presupuesto"
+    string_1 = "Selecciona tu presupuesto"
+
+    sd = StringDiff::Diff.new(string_1, string_2).diff
+
+    expect(sd).to eq("<span class='deletion'>Seleccione</span> <span class='insertion'>Selecciona</span> <span class='deletion'>su</span> <span class='insertion'>tu</span> presupuesto")
+  end
+
+  it 'should add a deletion directly before a word that replaces it' do
+    string_1= "Traductor mensaje de"
+    string_1 = "Envía el mensaje al traductor"
+
+    sd = StringDiff::Diff.new(string_1, string_2).diff
+
+    expect(sd).to eq("<span class="deletion">Traductor</span> <span class="insertion">Envía</span> mensaje <span class="deletion">de</span> <span class="insertion">el</span> <span class="insertion">al</span> <span class="insertion">traductor</span>")
+  end
+
 end
